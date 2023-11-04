9a802021aad9a071e5c2de57ebe6590ba2849bd3
GhofraneFerchchi <ghofranefer01@gmail.com>
2023-11-04 12:50:07 +0100

    Fixing errors

--- crud-tuto-front/src/app/shared/Service/Facture.service.ts
+++ crud-tuto-front/src/app/shared/Service/Facture.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FactureService {
-  readonly API_URL = 'http://localhost:80/SpringMVC/facture';
+  readonly API_URL = 'http://localhost:8088/SpringMVC/facture';
 
   constructor(private httpClient: HttpClient) { }
 
